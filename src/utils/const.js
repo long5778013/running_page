c678f431411989f3d5a6aaec12e3de3a5a0e8a28
dragonsong <songhuilong1024@outlook.com>
2021-01-30 19:16:35 +0800

    config: Update mapbox token

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,6 +1,6 @@
 // const
 const MAPBOX_TOKEN =
-  'pk.eyJ1IjoieWlob25nMDYxOCIsImEiOiJja2J3M28xbG4wYzl0MzJxZm0ya2Fua2p2In0.PNKfkeQwYuyGOTT_x9BJ4Q';
+  'sk.eyJ1IjoiZHJhZ29uc29uZyIsImEiOiJja2tqbWRoYjcwenFmMnVwZ3AwanQ3Y24xIn0.BPB0fiRXQJ0yCeroRfjsuA';
 const MUNICIPALITY_CITIES_ARR = [
   '北京市',
   '上海市',
